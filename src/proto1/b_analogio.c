ad157206846d75b2b5bc89a5f8bdf6ba07412bd1
niw@niw.at <niw@niw.at@7f1393f5-d94d-0410-8f0b-e354acbf0588>
2008-06-05 10:50:35 +0000

    * add sound stop method

--- src/proto1/b_analogio.c
+++ src/proto1/b_analogio.c
@@ -41,22 +41,42 @@ snd_global_method(JSVirtualMachine * vm, JSBuiltinInfo * builtin_info,
 	if (args->u.vinteger == 2
 		&& args[1].type == JS_INTEGER
 		&& args[1].u.vinteger >= 0
-		&& args[1].u.vinteger < 2
-		&& args[2].type == JS_INTEGER
-		&& args[2].u.vinteger >= 245) {
-		switch(args[1].u.vinteger) {
-		case 0:
-			if(! SOUND_is_inited(0)) {
-				SOUND_init(0);
+		&& args[1].u.vinteger < 2) {
+		JSFloat f;
+		if(args[2].type == JS_INTEGER) {
+			f = (JSFloat)args[2].u.vinteger;
+		} else if (args[2].type == JS_FLOAT) {
+			f = args[2].u.vfloat;
+		} else {
+			return;
+		}
+		if(f == 0) {
+			switch(args[1].u.vinteger) {
+			case 0:
+				SOUND_off(0);
+				break;
+			case 1:
+				SOUND_off(1);
+				break;
 			}
-			SOUND_out(0,args[2].u.vinteger);
-			break;
-		case 1:
-			if(! SOUND_is_inited(1)) {
-				SOUND_init(1);
+		} else {
+			if(f<245) {
+				f = 245;
 			}
-			SOUND_out(1,args[2].u.vinteger);
-			break;
+			switch(args[1].u.vinteger) {
+			case 0:
+				if(! SOUND_is_inited(0)) {
+					SOUND_init(0);
+				}
+				SOUND_out(0,f);
+				break;
+			case 1:
+				if(! SOUND_is_inited(1)) {
+					SOUND_init(1);
+				}
+				SOUND_out(1,f);
+				break;
+			}
 		}
 		result_return->u.vboolean = 1;
 	}
