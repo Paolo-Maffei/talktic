49abb7b0a305a0dcbe4028ea3b670788f18d1b2f
niw@niw.at <niw@niw.at@7f1393f5-d94d-0410-8f0b-e354acbf0588>
2008-06-05 10:44:33 +0000

    * remove unused files

--- trunk/src/delay.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DELAY_H__
-#define __DELAY_H__
-#define F_CPU MOXA_MCU_CLOCK
-#include <util/delay.h>
-#define delay_us(t) _delay_ms(t/1000)
-#define delay_ms(t) { uint16_t dt=(t); while(dt--) { _delay_ms(1); } }
-#endif
