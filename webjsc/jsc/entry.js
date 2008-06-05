6a69e27e91e205903c21b7c2fa4fef788d07b98c
niw@niw.at <niw@niw.at@7f1393f5-d94d-0410-8f0b-e354acbf0588>
2008-06-05 10:55:37 +0000

    * retab!

--- webjsc/jsc/entry.js
+++ webjsc/jsc/entry.js
@@ -59,118 +59,118 @@ JSC$FLAG_WARN_MASK = 0xffff0000;
 
 function JSC$compile_file(fname, flags, asm_file, bc_file)
 {
-    var stream = new JSC$StreamFile(fname);
-    return JSC$compile_stream(stream, flags, asm_file, bc_file);
+	var stream = new JSC$StreamFile(fname);
+	return JSC$compile_stream(stream, flags, asm_file, bc_file);
 }
 
 
 function JSC$compile_string(str, flags, asm_file, bc_file)
 {
-    var stream = new JSC$StreamString(str);
-    return JSC$compile_stream(stream, flags, asm_file, bc_file);
+	var stream = new JSC$StreamString(str);
+	return JSC$compile_stream(stream, flags, asm_file, bc_file);
 }
 
 
 function JSC$compiler_reset()
 {
-    /* Reset compiler to a known initial state. */
-    JSC$parser_reset();
-    JSC$gram_reset();
-    JSC$asm_reset();
+	/* Reset compiler to a known initial state. */
+	JSC$parser_reset();
+	JSC$gram_reset();
+	JSC$asm_reset();
 }
 
 
 function JSC$compile_stream(stream, flags, asm_file, bc_file)
 {
-    var result = false;
+	var result = false;
 
-    JSC$compiler_reset();
+	JSC$compiler_reset();
 
-    if (stream.open()) {
-        try {
-            JSC$verbose = ((flags & JSC$FLAG_VERBOSE) != 0);
-            JSC$generate_debug_info = ((flags & JSC$FLAG_GENERATE_DEBUG_INFO) != 0);
+	if (stream.open()) {
+		try {
+			JSC$verbose = ((flags & JSC$FLAG_VERBOSE) != 0);
+			JSC$generate_debug_info = ((flags & JSC$FLAG_GENERATE_DEBUG_INFO) != 0);
 
-            JSC$warn_unused_argument = ((flags & JSC$FLAG_WARN_UNUSED_ARGUMENT) != 0);
-            JSC$warn_unused_variable = ((flags & JSC$FLAG_WARN_UNUSED_VARIABLE) != 0);
-            JSC$warn_shadow = ((flags & JSC$FLAG_WARN_SHADOW) != 0);
-            JSC$warn_with_clobber = ((flags & JSC$FLAG_WARN_WITH_CLOBBER) != 0);
-            JSC$warn_missing_semicolon = ((flags & JSC$FLAG_WARN_MISSING_SEMICOLON) != 0);
-            JSC$warn_strict_ecma = ((flags & JSC$FLAG_WARN_STRICT_ECMA) != 0);
-            JSC$warn_deprecated = ((flags & JSC$FLAG_WARN_DEPRECATED) != 0);
+			JSC$warn_unused_argument = ((flags & JSC$FLAG_WARN_UNUSED_ARGUMENT) != 0);
+			JSC$warn_unused_variable = ((flags & JSC$FLAG_WARN_UNUSED_VARIABLE) != 0);
+			JSC$warn_shadow = ((flags & JSC$FLAG_WARN_SHADOW) != 0);
+			JSC$warn_with_clobber = ((flags & JSC$FLAG_WARN_WITH_CLOBBER) != 0);
+			JSC$warn_missing_semicolon = ((flags & JSC$FLAG_WARN_MISSING_SEMICOLON) != 0);
+			JSC$warn_strict_ecma = ((flags & JSC$FLAG_WARN_STRICT_ECMA) != 0);
+			JSC$warn_deprecated = ((flags & JSC$FLAG_WARN_DEPRECATED) != 0);
 
-            /* Compilation and assembler generation time optimizations. */
-            JSC$optimize_constant_folding = true;
-            JSC$optimize_type = true;
+			/* Compilation and assembler generation time optimizations. */
+			JSC$optimize_constant_folding = true;
+			JSC$optimize_type = true;
 
-            JSC$parser_parse(stream);
+			JSC$parser_parse(stream);
 
-            /* Assembler. */
-            JSC$asm_generate();
+			/* Assembler. */
+			JSC$asm_generate();
 
-            /*
-             * We don't need the syntax tree anymore.  Free it and save
-             * some memory.
-             */
-            JSC$parser_reset();
+			/*
+			 * We don't need the syntax tree anymore.  Free it and save
+			 * some memory.
+			 */
+			JSC$parser_reset();
 
-            /* Optimize if requested. */
-            if ((flags & JSC$FLAG_OPTIMIZE_MASK) != 0)
-                JSC$asm_optimize(flags);
+			/* Optimize if requested. */
+			if ((flags & JSC$FLAG_OPTIMIZE_MASK) != 0)
+				JSC$asm_optimize(flags);
 
-            if (typeof asm_file == "string") {
-                var asm_stream = new File(asm_file);
-                var src_stream = false;
+			if (typeof asm_file == "string") {
+				var asm_stream = new File(asm_file);
+				var src_stream = false;
 
-                if (asm_stream.open("w")) {
-                    if ((flags & JSC$FLAG_ANNOTATE_ASSEMBLER) != 0)
-                        if (stream.rewind())
-                            src_stream = stream;
+				if (asm_stream.open("w")) {
+					if ((flags & JSC$FLAG_ANNOTATE_ASSEMBLER) != 0)
+						if (stream.rewind())
+							src_stream = stream;
 
-                    JSC$asm_print(src_stream, asm_stream);
-                    asm_stream.close();
-                } else
-                    JSC$message("jsc: couldn't create asm output file \""
-                                + asm_file + "\": " + System.strerror(System.errno));
-            }
+					JSC$asm_print(src_stream, asm_stream);
+					asm_stream.close();
+				} else
+					JSC$message("jsc: couldn't create asm output file \""
+								+ asm_file + "\": " + System.strerror(System.errno));
+			}
 
-            JSC$asm_finalize();
+			JSC$asm_finalize();
 
-            result = JSC$asm_bytecode();
+			result = JSC$asm_bytecode();
 
-            /* Remove all intermediate results from the memory. */
-            JSC$compiler_reset();
+			/* Remove all intermediate results from the memory. */
+			JSC$compiler_reset();
 
-            if (typeof bc_file == "string") {
-                var ostream = new File(bc_file);
-                if (ostream.open("w")) {
-                    ostream.write(result);
-                    ostream.close();
+			if (typeof bc_file == "string") {
+				var ostream = new File(bc_file);
+				if (ostream.open("w")) {
+					ostream.write(result);
+					ostream.close();
 
-                    if ((flags & JSC$FLAG_GENERATE_EXECUTABLE_BC_FILES) != 0) {
-                        /* Add execute permissions to the output file. */
-                        var st = File.stat(bc_file);
-                        if (st) {
-                            if (!File.chmod(bc_file, st[2] | 0111))
-                                JSC$message("jsc: couldn't add execute "
-                                            + "permissions to bc file \""
-                                            + bc_file + "\": " + System.strerror(System.errno));
-                        } else
-                            JSC$message("jsc: couldn't stat bc file \"" + bc_file
-                                        + "\": " + System.strerror(System.errno));
-                    }
-                } else
-                    JSC$message("jsc: couldn't create bc file \"" + bc_file
-                                + "\": " + System.strerror(System.errno));
-            }
-        }
-        finally {
-            stream.close();
-        }
-    } else
-        error("jsc: couldn't open input stream \"" + stream.name + "\": " + stream.error);
+					if ((flags & JSC$FLAG_GENERATE_EXECUTABLE_BC_FILES) != 0) {
+						/* Add execute permissions to the output file. */
+						var st = File.stat(bc_file);
+						if (st) {
+							if (!File.chmod(bc_file, st[2] | 0111))
+								JSC$message("jsc: couldn't add execute "
+											+ "permissions to bc file \""
+											+ bc_file + "\": " + System.strerror(System.errno));
+						} else
+							JSC$message("jsc: couldn't stat bc file \"" + bc_file
+										+ "\": " + System.strerror(System.errno));
+					}
+				} else
+					JSC$message("jsc: couldn't create bc file \"" + bc_file
+								+ "\": " + System.strerror(System.errno));
+			}
+		}
+		finally {
+			stream.close();
+		}
+	} else
+		error("jsc: couldn't open input stream \"" + stream.name + "\": " + stream.error);
 
-    return result;
+	return result;
 }
 
 
--- webjsc/jsc/namespace.js
+++ webjsc/jsc/namespace.js
@@ -36,92 +36,92 @@ JSC$SCOPE_LOCAL = 2;
 
 function JSC$NameSpace()
 {
-    this.frame = new JSC$NameSpaceFrame();
-    this.push_frame = JSC$NameSpace_push_frame;
-    this.pop_frame = JSC$NameSpace_pop_frame;
-    this.alloc_local = JSC$NameSpace_alloc_local;
-    this.define_symbol = JSC$NameSpace_define_symbol;
-    this.lookup_symbol = JSC$NameSpace_lookup_symbol;
+	this.frame = new JSC$NameSpaceFrame();
+	this.push_frame = JSC$NameSpace_push_frame;
+	this.pop_frame = JSC$NameSpace_pop_frame;
+	this.alloc_local = JSC$NameSpace_alloc_local;
+	this.define_symbol = JSC$NameSpace_define_symbol;
+	this.lookup_symbol = JSC$NameSpace_lookup_symbol;
 }
 
 
 function JSC$NameSpace_push_frame()
 {
-    var f = new JSC$NameSpaceFrame();
+	var f = new JSC$NameSpaceFrame();
 
-    f.num_locals = this.frame.num_locals;
+	f.num_locals = this.frame.num_locals;
 
-    f.next = this.frame;
-    this.frame = f;
+	f.next = this.frame;
+	this.frame = f;
 }
 
 
 function JSC$NameSpace_pop_frame()
 {
-    var i;
+	var i;
 
-    for (i = this.frame.defs; i != null; i = i.next)
-        if (i.usecount == 0) {
-            if (i.scope == JSC$SCOPE_ARG) {
-                if (JSC$warn_unused_argument)
-                    JSC$warning(JSC$filename + ":" + i.linenum.toString()
-                                + ": warning: unused argument `" + i.symbol + "'");
-            } else {
-                if (JSC$warn_unused_variable)
-                    JSC$warning(JSC$filename + ":" + i.linenum.toString()
-                                + ": warning: unused variable `" + i.symbol + "'");
-            }
-        }
+	for (i = this.frame.defs; i != null; i = i.next)
+		if (i.usecount == 0) {
+			if (i.scope == JSC$SCOPE_ARG) {
+				if (JSC$warn_unused_argument)
+					JSC$warning(JSC$filename + ":" + i.linenum.toString()
+								+ ": warning: unused argument `" + i.symbol + "'");
+			} else {
+				if (JSC$warn_unused_variable)
+					JSC$warning(JSC$filename + ":" + i.linenum.toString()
+								+ ": warning: unused variable `" + i.symbol + "'");
+			}
+		}
 
-    this.frame = this.frame.next;
+	this.frame = this.frame.next;
 }
 
 
 function JSC$NameSpace_alloc_local()
 {
-    return this.frame.num_locals++;
+	return this.frame.num_locals++;
 }
 
 
 function JSC$NameSpace_define_symbol(symbol, scope, value, linenum)
 {
-    var i;
+	var i;
 
-    for (i = this.frame.defs; i != null; i = i.next)
-        if (i.symbol == symbol) {
-            if (i.scope == scope)
-                error(JSC$filename + ":" + i.linenum.toString()
-                      + ": redeclaration of `" + i.symbol + "'");
-            if (i.scope == JSC$SCOPE_ARG && JSC$warn_shadow)
-                JSC$warning(JSC$filename + ":" + linenum.toString()
-                            + ": warning: declaration of `" + symbol + "' shadows a parameter");
+	for (i = this.frame.defs; i != null; i = i.next)
+		if (i.symbol == symbol) {
+			if (i.scope == scope)
+				error(JSC$filename + ":" + i.linenum.toString()
+					  + ": redeclaration of `" + i.symbol + "'");
+			if (i.scope == JSC$SCOPE_ARG && JSC$warn_shadow)
+				JSC$warning(JSC$filename + ":" + linenum.toString()
+							+ ": warning: declaration of `" + symbol + "' shadows a parameter");
 
-            i.scope = scope;
-            i.value = value;
-            i.linenum = linenum;
+			i.scope = scope;
+			i.value = value;
+			i.linenum = linenum;
 
-            return;
-        }
+			return;
+		}
 
-    /* Create a new definition. */
-    i = new JSC$SymbolDefinition(symbol, scope, value, linenum);
-    i.next = this.frame.defs;
-    this.frame.defs = i;
+	/* Create a new definition. */
+	i = new JSC$SymbolDefinition(symbol, scope, value, linenum);
+	i.next = this.frame.defs;
+	this.frame.defs = i;
 }
 
 
 function JSC$NameSpace_lookup_symbol(symbol)
 {
-    var f, i;
+	var f, i;
 
-    for (f = this.frame; f != null; f = f.next)
-        for (i = f.defs; i != null; i = i.next)
-            if (i.symbol == symbol) {
-                i.usecount++;
-                return i;
-            }
+	for (f = this.frame; f != null; f = f.next)
+		for (i = f.defs; i != null; i = i.next)
+			if (i.symbol == symbol) {
+				i.usecount++;
+				return i;
+			}
 
-    return null;
+	return null;
 }
 
 /*
@@ -130,20 +130,20 @@ function JSC$NameSpace_lookup_symbol(symbol)
 
 function JSC$NameSpaceFrame()
 {
-    this.next = null;
-    this.defs = null;
-    this.num_locals = 0;
+	this.next = null;
+	this.defs = null;
+	this.num_locals = 0;
 }
 
 
 function JSC$SymbolDefinition(symbol, scope, value, linenum)
 {
-    this.next = null;
-    this.symbol = symbol;
-    this.scope = scope;
-    this.value = value;
-    this.linenum = linenum;
-    this.usecount = 0;
+	this.next = null;
+	this.symbol = symbol;
+	this.scope = scope;
+	this.value = value;
+	this.linenum = linenum;
+	this.usecount = 0;
 }
 
 
