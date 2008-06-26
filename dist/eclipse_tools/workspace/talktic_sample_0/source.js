2dfd8e5f411bc897a4fc87e1dd304236745ba685
niw@niw.at <niw@niw.at@7f1393f5-d94d-0410-8f0b-e354acbf0588>
2008-06-26 12:25:37 +0000

    * add eclipse_tools

---
+++ dist/eclipse_tools/workspace/talktic_sample_0/source.js
@@ -0,0 +1,12 @@
+// sample-00.js 
+// MOXA: 最初のプログラム 
+
+// 初期化処理
+pinMode(0, true); // PORT0  を出力に設定 (第2引数がtrue だと出力設定)
+
+// LED 点灯(注:LOW  出力でLED 点灯) 
+digitalWrite(0, false); 
+
+// ダミーループ
+while(true) { 
+}
